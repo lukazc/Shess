459936633b547a2a767d1d0021f91325a2767e0c
Zvonimir Petrovic <zvonimir.petrovic10@gmail.com>
2018-06-03 18:29:37 +0200

    SplashScreen fully working

--- src/main/java/com/lukazc/gui/Splash.java
+++ src/main/java/com/lukazc/gui/Splash.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 
 public class Splash extends JFrame{
 
-    JFrame splash = new JFrame();
+    private JFrame splash = new JFrame();
     private static String vid;
 
     public Splash(){
@@ -14,16 +14,17 @@ public class Splash extends JFrame{
         splash.setSize(500, 500);
         splash.setLocationRelativeTo(null); // Center window
         splash.setResizable(false);
-        splash.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        splash.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         splash.getContentPane().add(panel);
+        splash.setAlwaysOnTop(true);
         JLabel label = new JLabel(video);
         panel.add(label);
         splash.setVisible(false);
     }
-    public void display(){
+    void display(){
         splash.setVisible(true);
     }
-    public void exitSplash(){
+    void exitSplash(){
         splash.setVisible(false);
     }
 }
